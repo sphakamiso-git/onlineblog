c9850a9f514f27541ac65dd28a8cf43d0af7d1b7
sphakamiso <sparngobese@gmail.com>
2023-03-13 22:45:56 +0200

    first commit and working with templates

---
+++ blog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+posts = [
+    {'author' : 'CoreyMS', 'title': 'Blog Post 1', 'content' : 'First post content', 'date_posted' : 'August 27 2024'},
+    {'author' : 'Jane Doe', 'title': 'Blog Post 1', 'content' : 'First post content', 'date_posted' : 'August 23 2024'}
+]
+
+def home(request):
+    context = {
+        'posts' : posts
+    }
+    return render(request,'blog/home.html', context)
+
+def about(request):
+    return render(request,'blog/about.html')
